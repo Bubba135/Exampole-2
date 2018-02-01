9222d8adba8e710d4d6646a172f56e1a0b53adfb
CRISTIAN FLORES-FOSTER <CRIFLORE@uat.edu>
2018-02-01 16:57:22 -0700

    Add project files.

---
+++ Exampole 2/Exampole 2.cpp
@@ -0,0 +1,24 @@
+// Exampole 2.cpp : Defines the entry point for the console application.
+//exampole 2
+
+#include "stdafx.h"
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+
+
+	int x = 1;
+
+	do {
+		cout << x << endl;
+		x++;
+	} while (x < 10);
+
+
+
+    return 0;
+}
+
